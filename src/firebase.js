0af389ed5363c3cfbc86fa36860a2a27a6d8b4ee
Kripton0810 <subhankar0810@gmail.com>
2022-07-04 15:05:07 +0530

    firebae

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,23 @@
-// import firebase from "firebase";
-import * as firebase from "firebase";
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-    apiKey: "AIzaSyAKQBpFhM-gDwH1AHLclwDeZioHGrWp3zE",
-    authDomain: "disneyplus-clone-f8d5c.firebaseapp.com",
-    projectId: "disneyplus-clone-f8d5c",
-    storageBucket: "disneyplus-clone-f8d5c.appspot.com",
-    messagingSenderId: "972990256615",
-    appId: "1:972990256615:web:6059202bbb1f02f4625942",
-    measurementId: "G-JPCP6B1ZK0"
-  };
-  
-  // Initialize Firebase
-  // const app = initializeApp(firebaseConfig);
-  // const analytics = getAnalytics(app);
-const firebaseApp = firebase.initializeApp(firebaseConfig); 
-const db = firebaseApp.firestore();
-const auth = firebase.auth();
-const provider = new firebase.auth.GoogleAuthProvider();
-const storage = firebase.storage();
+  apiKey: "AIzaSyAKQBpFhM-gDwH1AHLclwDeZioHGrWp3zE",
+  authDomain: "disneyplus-clone-f8d5c.firebaseapp.com",
+  projectId: "disneyplus-clone-f8d5c",
+  storageBucket: "disneyplus-clone-f8d5c.appspot.com",
+  messagingSenderId: "972990256615",
+  appId: "1:972990256615:web:6059202bbb1f02f4625942",
+  measurementId: "G-JPCP6B1ZK0",
+};
 
-export { auth, provider, storage };
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore();
+const auth = getAuth(firebaseApp);
+const storage = getStorage();
+// const provider = GoogleAuthProvider();
+
+// const googleProvider = new firebase.auth.GoogleAuthProvider();
+export { auth, firebaseApp, storage };
 export default db;
