5a4c5365ecc26c32ccf346cdb1d1c7c8dd39a356
Kripton0810 <subhankar0810@gmail.com>
2022-07-23 18:08:37 +0530

    subhankar changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,7 @@ import { useDispatch, useSelector } from "react-redux";
 import db from "../firebase";
 import { setMovies } from "../features/movie/movieSlice";
 import { selectUserName } from "../features/user/userSlice";
+import { collection, onSnapshot, query } from "firebase/firestore";
 
 const Home = (props) => {
   const dispatch = useDispatch();
@@ -20,38 +21,33 @@ const Home = (props) => {
   let trending = [];
 
   useEffect(() => {
-    console.log("hello");
-    db.collection("movies").onSnapshot((snapshot) => {
-      snapshot.docs.map((doc) => {
-        console.log(recommends);
-        switch (doc.data().type) {
-          case "recommend":
-            recommends = [...recommends, { id: doc.id, ...doc.data() }];
-            break;
-
-          case "new":
-            newDisneys = [...newDisneys, { id: doc.id, ...doc.data() }];
-            break;
-
-          case "original":
-            originals = [...originals, { id: doc.id, ...doc.data() }];
-            break;
-
-          case "trending":
-            trending = [...trending, { id: doc.id, ...doc.data() }];
-            break;
+    const q = query(collection(db, "movies"));
+    onSnapshot(q, (querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(doc.data());
+        const data = doc.data();
+        if (data.type === "recommend") {
+          recommends.push(data);
+        }
+        if (data.type === "new") {
+          newDisneys.push(data);
+        }
+        if (data.type === "original") {
+          originals.push(data);
+        }
+        if (data.type === "trending") {
+          trending.push(data);
         }
       });
-
-      dispatch(
-        setMovies({
-          recommend: recommends,
-          newDisney: newDisneys,
-          original: originals,
-          trending: trending,
-        })
-      );
     });
+    dispatch(
+      setMovies({
+        recommend: recommends,
+        newDisney: newDisneys,
+        original: originals,
+        trending: trending,
+      })
+    );
   }, [userName]);
 
   return (
